28963e47f47daac9644579784288812ee8860619
Joao Pedro <joaopedrowatson@hotmail.com>
2021-09-11 21:20:30 -0300

    a

---
+++ layout/HomeLayout/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from "./styles";
+
+const HomeLayout = () => {
+  return(
+    <Box>
+      <h1>Sexo</h1>
+    </Box>
+  )
+}
+
+export default HomeLayout
---
+++ layout/HomeLayout/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
